fb5ae39c2d51d03df20fa14305c10be2881869ed
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-02-25 23:13:51 +0530

    useState React-hook added to  Board.js. onClick props added to Sqaure.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  // Initializing the state of the board : 9 boxes are null
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  // Creating a function to handle a square button
+  const handleSquareButton = position => {
+    //console.log(position);
+  };
+
+  // Render a square button
+  const renderSquareButton = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareButton(position);
+        }}
+      />
+    );
+  };
+
   return (
     // 3 x 3 Board
     <div className="board">
       {/* First Row*/}
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquareButton(0)}
+        {renderSquareButton(1)}
+        {renderSquareButton(2)}
       </div>
       {/* Second Row*/}
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquareButton(3)}
+        {renderSquareButton(4)}
+        {renderSquareButton(5)}
       </div>
       {/* Third Row*/}
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquareButton(6)}
+        {renderSquareButton(7)}
+        {renderSquareButton(8)}
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 // Square component receives argument - props (object)
-// props.value is destructured as { value }
-const Square = ({ value }) => {
+// props.value is destructured as { value } and props.onClick as { onClick }
+const Square = ({ value, onClick }) => {
   // Return 1 square clickable box - button
-  return <button className="square">{value}</button>; // access "value" attribute in JS using {} exp
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  ); // access "value" attribute in JS using {} exp
 };
 
 export default Square;
