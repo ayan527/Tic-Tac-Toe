9d22178e392116ddb6e1d4bbd5465e0eb3edd326
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-02-26 22:42:45 +0530

    helper.js is added to select winner.state shared between App & Board components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helper';
 
-// import styling sheet
+// Import styling sheet
 import './styles/root.scss';
 
 const App = () => {
+  // Initializing the state of the board : 9 boxes are null
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  // Function gets called everytime the component gets rerendered with updated board
+  const winner = calculateWinner(board);
+
+  // Initializing the current state of the board
+  const [isXNext, setXNext] = useState(true);
+
+  // Set the message based on the state of the board
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next is ${isXNext ? 'X' : 'O'}`;
+
+  // Creating a function to handle a square button
+  const handleSquareButton = position => {
+    //console.log(position);
+
+    // Check if the position is already modified or Winner is there
+    if (board[position] || winner) {
+      return;
+    }
+
+    // Update the square box of board
+    // 'previous' is the last updated state of the board : Array of size 9
+    setBoard(previous => {
+      // changing the current state of the clicked box
+      return previous.map((current, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        } else {
+          return current;
+        }
+      });
+    });
+
+    // Update the state of the board
+    setXNext(previous => !previous);
+  };
+
+  // Sharing of state between components through props
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={board} handleSquareButton={handleSquareButton} />
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,39 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  // Initializing the state of the board : 9 boxes are null
-  const [board, setBoard] = useState(Array(9).fill(null));
-
-  // Initializing the current state of the board
-  const [isXNext, setXNext] = useState(true);
-
-  // Creating a function to handle a square button
-  const handleSquareButton = position => {
-    //console.log(position);
-
-    // check if the position is already modified
-    if (board[position]) {
-      return;
-    }
-
-    // update the square box of board
-    // 'previous' is the last updated state of the board : Array of size 9
-    setBoard(previous => {
-      // changing the current state of the clicked box
-      return previous.map((current, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        } else {
-          return current;
-        }
-      });
-    });
-
-    //update the state of the board
-    setXNext(previous => !previous);
-  };
-
+const Board = ({ board, handleSquareButton }) => {
   // Render a square button
   const renderSquareButton = position => {
     return (
