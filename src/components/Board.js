adc168d1d2a84b775bc1caf626b7d88e7f452eda
Ayan Mukherjee <ayanmukherjee20102011@gmail.com>
2021-02-26 09:53:48 +0530

    handleSquareButton func updated. X and O values are added.

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,33 @@ const Board = () => {
   // Initializing the state of the board : 9 boxes are null
   const [board, setBoard] = useState(Array(9).fill(null));
 
+  // Initializing the current state of the board
+  const [isXNext, setXNext] = useState(true);
+
   // Creating a function to handle a square button
   const handleSquareButton = position => {
     //console.log(position);
+
+    // check if the position is already modified
+    if (board[position]) {
+      return;
+    }
+
+    // update the square box of board
+    // 'previous' is the last updated state of the board : Array of size 9
+    setBoard(previous => {
+      // changing the current state of the clicked box
+      return previous.map((current, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        } else {
+          return current;
+        }
+      });
+    });
+
+    //update the state of the board
+    setXNext(previous => !previous);
   };
 
   // Render a square button
